31ac11ffb9039ef58b5cfaa7caa43cf9ed92f75e
Brionna McElhaney <brionnapartee@gmail.com>
2021-10-19 10:10:15 -0500

    create chirp page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,66 @@
-import React, { useEffect, useState } from "react";
-import Greeter from './Greeter';
+import React, { useState } from "react";
+import './chirp.css';
+import Tweet from './Tweet';
 
-
+let myChirp = [];
 const App = () => {
-    const [username, setUsername] = useState(' ');
-    const [loaded, setLoaded] = useState(false);
-useEffect(() => {
-    setTimeout(() => {
-        setLoaded({loaded: true});
-      }, [3000]);
-})
+const [username, setUsername] = useState('');
+const [someText, setsomeText] = useState(' ');
 
-function handleClick() {
-    setLoaded({loaded: true});
-}
-if(!loaded){
-return (
-<><h1>Website Loading...</h1>
-<button onClick={handleClick}>Please Click me!</button>
-</>
-)
+
+function handleClick(e) {
+    e.preventDefault();
+    myChirp.push({ 'myName': username, 'myChirp': someText });
+    setUsername('');
+    setsomeText('');
 }
 
-    return(
-        <div>
-           <Greeter phrase="Howdy!" name="Brionna" /> 
-           <Greeter phrase="Well Hello" name="Brionna" />
-           <Greeter phrase="Hi" name = "Brionna" />
-           <input value={username} onChange={e => setUsername(e.target.value)}/>
-           <p>You are logging in as:{username} </p>
+function List() {
+    const itemList = myChirp.map((item, index) => (
+      <li key={index}>
+          <div className="card"  id="beDone">
+          <div className="card-header">
+          {item.myName}
         </div>
+        <div className="card-body">
+          <p className="card-text">{item.myChirp}</p>
+          </div>
+          </div>
+      </li>
+    ));
+    return (
+      <div>
+        <ol style={{ listStyleType: "none" }}>{itemList}</ol>
+      </div>
     );
+  }
+  
+
+    return (
+        <>
+        <h1 id="title">What's up my peoples!!!</h1>
+        <form>
+        <div id="superDome">
+            
+            
+            <label>
+                Username
+            <input value={username} onChange={e => setUsername(e.target.value)}/>
+            </label>
+            <label>
+                whatUSay
+            <input value={someText} onChange={e => setsomeText(e.target.value)}/>
+            </label>
+            
+            <button id="instaChirp" onClick={handleClick}>Submit</button>
+        </div>
+        </form>
+ 
+    <Tweet />
+    <List />
+    </>
+    );
+
 }
 
 export default App;
---
+++ src/components/Tweet.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Tweet = () => {
+    return(
+        
+        <div className="card">
+        <div className="card-header">
+          @cutieTootie
+        </div>
+        <div className="card-body">
+          <p className="card-text">Peace up A town down</p>
+          </div>
+          <div className="card-header">
+          @stickyFingers101
+        </div>
+        <div className="card-body">
+          <p className="card-text2">If you love me say it back</p>
+          </div>
+          <div className="card-header">
+          @hotTottyOG
+        </div>
+        <div className="card-body">
+          <p className="card-text">Hello my pretties!!</p>
+          </div>
+          </div>
+          
+    )
+}
+export default Tweet;
